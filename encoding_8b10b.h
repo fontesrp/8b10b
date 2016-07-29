331b3f53a15765829f56e1d147328da9acef63d3
fontesrp <fontesr2@gmail.com>
2016-07-28 22:24:59 -0300

    Add encoding_8b10b.h, for better organize the code. Update encoding_8b10b.ino, removed all old testing code (table is already correct), added new code for testing communication between boards.

---
+++ encoding_8b10b.h
@@ -0,0 +1,7 @@
+#ifndef encoding_8b10b_h
+#define encoding_8b10b_h
+
+unsigned int encode8B10B(unsigned char data); // encode.ino
+void setup8B10B(); // encode.ino
+
+#endif
--- encoding_8b10b.ino
+++ encoding_8b10b.ino
@@ -1,70 +1,25 @@
-/* ---------- Variáveis globais necessárias para a codificação ---------- */
-unsigned int searchTable[256][3];
-char rd;
-/* ---------------------------------------------------------------------- */
-
-unsigned int inX, inY, input8b;
+#include "encoding_8b10b.h"
 
 void setup() {
-  
-  setup8B10B();
-  inX = inY = 0;
-  Serial.begin(9600);
-  Serial.print("    \t Y \t  X  \tCURRENT RD- \tCURRENT RD+ \t  \n");
-  Serial.print("DX.Y\tHGF\tEDCBA\tabcdei\tfghj\tabcdei\tfghj\tRD-\tRD+\n");
+	setup8B10B();
+	randomSeed(analogRead(0));
 }
 
 void loop() {
-  
-  unsigned int input, output;
-  unsigned int a, f;
-  char r;
 
-  Serial.print('D');
-  Serial.print(inX, DEC);
-  Serial.print('.');
-  Serial.print(inY, DEC);
-  Serial.print('\t');
-  Serial.print(inY, BIN);
-  Serial.print('\t');
-  Serial.print(inX, BIN);
-  Serial.print('\t');
+	unsigned char input;
+	unsigned int output;
 
-  rd = -1;
-  input = (inY << 5) | inX;
-  output = encode8B10B(input);
+	input = random(0x00, 0xFF);
+	output = encode8B10B(input);
 
-  a = (output & 0x3F0) >> 4;
-  f = output & 0xF;
+	Serial.print("main loop: input = ");
+	Serial.print(input, DEC);
+	Serial.println("");
+	Serial.print("main loop: output = ");
+	Serial.print(output, DEC);
+	Serial.println("");
 
-  Serial.print(a, BIN);
-  Serial.print('\t');
-  Serial.print(f, BIN);
-  Serial.print('\t');
-
-  r = rd = 1;
-  output = encode8B10B(input);
-
-  a = (output & 0x3F0) >> 4;
-  f = output & 0xF;
-
-  Serial.print(a, BIN);
-  Serial.print('\t');
-  Serial.print(f, BIN);
-  Serial.print('\t');
-
-  if (r == rd)
-    Serial.print("same\n");
-  else
-    Serial.print("flip\n");
-
-  if (++inY == 8) {
-    inY = 0;
-    inX++;
-  }
-  
-  if (inX >= 32)
-    while (1)
-      ;
+	delay(10000);
 }
 
